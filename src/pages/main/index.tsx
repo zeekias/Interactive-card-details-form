359b8c6443adcbfd9e91d727c13377063f0cb849
zeekias <moraispark9@gmail.com>
2022-09-14 13:10:00 -0300

    Finalizado

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -5,7 +5,7 @@ import {
   Complete,
   Container,
   Content,
-  DivDesign,
+  AsideDesign,
   FormCard,
   FrontCard,
 } from "./styles";
@@ -18,34 +18,65 @@ interface iCardInfo {
   };
   cardCvc: string;
 }
-import frontCardImage from "../../images/bg-card-front.png";
-import logoCardImage from "../../images/card-logo.svg";
-import backCardImage from "../../images/bg-card-back.png";
-import completeIcon from "../../images/icon-complete.svg";
 
 export default function index() {
   const inicialValue = {
     cardNumber: "",
-    cardHolder: "João Albuquerque",
+    cardHolder: "",
     cardExp: {
-      mm: "00",
-      yy: "00",
+      mm: "",
+      yy: "",
     },
-    cardCvc: "123",
+    cardCvc: "",
   };
   const [cardsInformations, setCardsInformations] =
     useState<iCardInfo>(inicialValue);
 
-  const [show, setShow] = useState(false);
-
-  function handleShow(e: React.FormEvent<HTMLFormElement> | undefined) {
-    e ? e.preventDefault() : "";
+  const [show, setShow] = useState(true);
+  const [errors, setErrors] = useState([""]);
+  function handleShow() {
     setShow(!show);
   }
 
+  function validCard(e: React.FormEvent<HTMLFormElement>) {
+    e ? e.preventDefault() : "";
+    const auxErrors = [];
+    if (cardsInformations.cardHolder.length < 15) {
+      auxErrors.push(
+        'O campo "CardHolder"  deve ter no Minimio 15 letras'
+      );
+    }
+    if (cardsInformations.cardNumber.length < 16) {
+      auxErrors.push(
+        'O campo "Card Number"  deve ter no Minimio 16 números'
+      );
+    }
+    if (cardsInformations.cardExp.yy.length < 2) {
+      auxErrors.push(
+        'O campo "Exp. Date (YY)"  deve ter no Minimio 2 números'
+      );
+    }
+    if (cardsInformations.cardExp.mm.length < 2) {
+      auxErrors.push(
+        'O campo "Exp. Date (MM)"  deve ter no Minimio 2 números'
+      );
+    }
+    if (cardsInformations.cardCvc.length < 3) {
+      auxErrors.push('O campo "CVC" deve ter no Minimio 3 números');
+    }
+    if (auxErrors[0]) {
+      setErrors(auxErrors);
+      return;
+    }
+      handleShow();
+    
+  }
+
   function handleCardsInformations(newValue: string, changeType: string) {
+    let regex = /[a-z]/;
     switch (changeType) {
       case "cardNumber":
+        if (newValue.toLowerCase().match(regex)) return;
         const aux = newValue.split("");
         if (aux.length > 4) {
           aux.splice(4, 0, " ");
@@ -60,6 +91,8 @@ export default function index() {
         });
         break;
       case "cardHolder":
+        regex = /[0-9]/;
+        if (regex.test(newValue)) return;
         setCardsInformations({
           cardNumber: cardsInformations.cardNumber,
           cardHolder: newValue,
@@ -68,6 +101,7 @@ export default function index() {
         });
         break;
       case "mm":
+        if (regex.test(newValue)) return;
         setCardsInformations({
           cardNumber: cardsInformations.cardNumber,
           cardHolder: cardsInformations.cardHolder,
@@ -76,6 +110,7 @@ export default function index() {
         });
         break;
       case "yy":
+        if (regex.test(newValue)) return;
         setCardsInformations({
           cardNumber: cardsInformations.cardNumber,
           cardHolder: cardsInformations.cardHolder,
@@ -84,6 +119,7 @@ export default function index() {
         });
         break;
       case "cvc":
+        if (regex.test(newValue)) return;
         setCardsInformations({
           cardNumber: cardsInformations.cardNumber,
           cardHolder: cardsInformations.cardHolder,
@@ -95,12 +131,11 @@ export default function index() {
   }
   return (
     <Container>
-      <DivDesign />
+      <AsideDesign />
       <CardsContainer>
         <FrontCard>
-          <img src={frontCardImage} alt="Card" className="card-background" />
           <div className="card-info-container">
-            <img src={logoCardImage} alt="CardLogo" className="card-logo" />
+            <img src={"./img/card-logo.svg"} alt="CardLogo" className="card-logo" /> 
             <div className="card-info">
               <span className="card-number">
                 {cardsInformations.cardNumber.padStart(
@@ -121,7 +156,6 @@ export default function index() {
           </div>
         </FrontCard>
         <BackCard>
-          <img src={backCardImage} alt="backCard" className="card-background" />
           <div className="card-info-container">
             <span>{cardsInformations.cardCvc.padStart(3, "000")}</span>
           </div>
@@ -131,13 +165,16 @@ export default function index() {
       <Content>
         {!show ? (
           <Complete>
-            <img src={completeIcon} alt="complete" />
+            <img src="/img/icon-complete.svg" alt="complete" />
             <h3>THANK YOU!</h3>
             <p> We've added your card details </p>
-            <button className="continue-btn" onClick={()=>handleShow()}> Continue </button>
+            <button className="continue-btn" onClick={() => handleShow()}>
+              {" "}
+              Continue{" "}
+            </button>
           </Complete>
         ) : (
-          <FormCard onSubmit={(e) => handleShow(e)}>
+          <FormCard onSubmit={(e) => validCard(e)}>
             <label>
               <h2>CARDHOLDER NAME:</h2>
               <input
@@ -146,7 +183,10 @@ export default function index() {
                 onChange={(e) =>
                   handleCardsInformations(e.target.value, "cardHolder")
                 }
+                pattern="[A-Za-z].{15,}"
+                title="* Apenas Letras *"
                 maxLength={18}
+                minLength={15}
                 required
               />
             </label>
@@ -154,11 +194,14 @@ export default function index() {
               <h2>CARD NUMBER:</h2>
               <input
                 type="text"
-                placeholder="e.g.1234 5678 9123 0000"
+                placeholder="e.g.1234567891230000"
                 onChange={(e) =>
                   handleCardsInformations(e.target.value, "cardNumber")
                 }
                 maxLength={16}
+                minLength={16}
+                pattern="[0-9]{16}"
+                title="* Apenas números - sem espaços*"
                 required
               />
             </label>
@@ -172,9 +215,11 @@ export default function index() {
                       className="data"
                       placeholder="MM"
                       maxLength={2}
+                      minLength={2}
                       onChange={(e) =>
                         handleCardsInformations(e.target.value, "mm")
                       }
+                      pattern="[0-9]{2}"
                       required
                     />
                     <input
@@ -182,9 +227,11 @@ export default function index() {
                       className="data"
                       placeholder="YY"
                       maxLength={2}
+                      minLength={2}
                       onChange={(e) =>
                         handleCardsInformations(e.target.value, "yy")
                       }
+                      pattern="[0-9]{2}"
                       required
                     />
                   </div>
@@ -199,12 +246,14 @@ export default function index() {
                       handleCardsInformations(e.target.value, "cvc")
                     }
                     maxLength={3}
+                    minLength={3}
                     required
                   />
                 </label>
               </div>
             </label>
             <button className="submit-btn"> Confirm </button>
+            
           </FormCard>
         )}
       </Content>
--- src/pages/main/styles.ts
+++ src/pages/main/styles.ts
@@ -1,70 +1,93 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
+export const Container = styled.main`
   width: 100vw;
   height: 100vh;
   display: flex;
+  position: relative;
+  button:hover {
+    box-shadow: 5px 5px 15px -7px #000000;
+  }
+
+  @media (max-width: 688px) {
+    flex-direction: column;
+  }
 `;
 
-export const DivDesign = styled.div`
-  background-color: aqua;
+export const AsideDesign = styled.aside`
   width: 30%;
   height: 100vh;
   background-image: linear-gradient(#21082f, #451d42, #21082e);
+  background-size: cover;
+  background-position: center;
+  @media (max-width: 688px) {
+    width: 100%;
+    height: 30%;
+  }
 `;
 export const Content = styled.div`
   width: 70%;
   height: 100vh;
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
+  @media (max-width: 688px) {
+    width: 100%;
+    height: 50%;
+  }
 `;
 
 export const CardsContainer = styled.div`
-  position: fixed;
-  width: 650px;
-  height: 520px;
+  position: absolute;
+  width: 600px;
+  height: 500px;
   top: 20%;
-  left: 10%;
+  left: 5%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  @media (max-width: 688px) {
+    top: 5%;
+    left: auto;
+    width: 90%;
+    flex-direction: column-reverse;
+    height: 300px;
+    width: 100%;
+  }
 `;
 export const FrontCard = styled.div`
-  width: 100%;
-  height: 50%;
-  display: flex;
-  align-items: center;
-  justify-content: flex-start;
-  position: relative;
-  .card-background {
-    position: absolute;
-    z-index: 1;
-  }
+  background-image: url("/img/bg-card-front.png");
+  background-repeat: no-repeat;
+  background-size: cover;
+  width: 447px;
+  height: 250px;
+  border-radius: 8px;
   .card-info-container {
-    width: 100%;
-    height: 100%;
     display: flex;
     flex-direction: column;
-    align-items: flex-start;
-    gap: 60px;
-    padding: 20px;
-    position: absolute;
-    z-index: 2;
+    width: 100%;
+    height: 100%;
     .card-logo {
-      width: 80px;
+      margin: 20px;
+      width: 50px;
     }
     .card-info {
-      width: 60%;
       height: 100%;
       display: flex;
       flex-direction: column;
+      justify-content: flex-end;
       align-items: center;
-      justify-content: space-between;
+      gap: 50px;
       color: hsl(0, 0%, 100%);
       .card-number {
         max-width: 100%;
         font-size: 32px;
       }
       .card-name-and-exp {
-        width: 100%;
+        width: 90%;
+        margin-bottom: 10px;
         display: flex;
         align-items: center;
         justify-content: space-between;
@@ -79,20 +102,62 @@ export const FrontCard = styled.div`
       }
     }
   }
+  @media (max-width: 688px) {
+    width: 80%;
+    height: 60%;
+    margin-left: 30px;
+    margin-top: -20px;
+
+    z-index: 99;
+    .card-info-container {
+      display: flex;
+      padding: 20px;
+      .card-logo {
+        padding: 0;
+        width: 40px;
+      }
+      .card-info {
+        gap: 10px;
+        .card-number {
+          width: 100%;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          font-size: 22px;
+        }
+        .card-name-and-exp {
+          width: 100%;
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+          padding-bottom: 20px;
+
+          .cardholder {
+            max-height: 20px;
+            max-width: 250px;
+            word-wrap: break-word;
+            overflow: hidden;
+          }
+        }
+      }
+    }
+  }
 `;
 
 export const BackCard = styled.div`
-  width: 100%;
-  height: 50%;
+  background-image: url("/img/bg-card-back.png");
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  width: 447px;
+  height: 250px;
   display: flex;
+  margin-top: -10px;
+  margin-left: 120px;
   align-items: center;
   justify-content: flex-end;
   position: relative;
   color: white;
-  .card-background {
-    position: absolute;
-    z-index: 1;
-  }
   .card-info-container {
     width: 100%;
     height: 100%;
@@ -100,9 +165,18 @@ export const BackCard = styled.div`
     flex-direction: column;
     align-items: flex-end;
     position: absolute;
-    z-index: 2;
     right: 10%;
     top: 45%;
+  }
+  @media (max-width: 688px) {
+    margin-left: 0;
+    width: 80%;
+    height: 200px;
+    border-radius: 8px;
+    .card-info-container {
+      right: 10%;
+      top: 42%;
+    }
   }
 `;
 
@@ -110,7 +184,7 @@ export const FormCard = styled.form`
   width: 50%;
   display: flex;
   flex-direction: column;
-  padding-left: 150px;
+  padding-left: 100px;
   gap: 20px;
   label {
     width: 100%;
@@ -149,6 +223,31 @@ export const FormCard = styled.form`
     letter-spacing: 0.1em;
     cursor: pointer;
   }
+
+  @media (max-width: 688px) {
+    width: 100vw;
+    padding-left: 0px;
+    padding-top: 52%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    label {
+      width: 80%;
+      div {
+        gap: 20px;
+        .data {
+          width: 80px;
+        }
+        .cvc {
+          width: 100px;
+        }
+      }
+    }
+
+    .submit-btn {
+      width: 80%;
+    }
+  }
 `;
 
 export const Complete = styled.div`
@@ -162,11 +261,15 @@ export const Complete = styled.div`
   gap: 20px;
   .continue-btn {
     width: 300px;
-    height: 50px;
+    min-height: 50px;
     border-radius: 8px;
     background-color: #21082f;
     color: white;
     letter-spacing: 0.1em;
     cursor: pointer;
+  }
+  @media (max-width: 688px) {
+    padding-left: 0px;
+    padding-top: 150px;
   }
 `;
